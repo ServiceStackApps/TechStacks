b56bc4e7175940ace8db31be050e83f29c82cd1f
Darren Reid <layoric@gmail.com>
2014-11-26 14:23:17 +1100

    Deployment test

--- src/TechStacks/TechStacks/deploy.sh
+++ src/TechStacks/TechStacks/deploy.sh
@@ -96,6 +96,7 @@ fi
 "$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"  
 exitWithMessageOnError "Kudu Sync to Target failed"  
 
+
 ##################################################################################################################################
 
 # Post deployment stub
