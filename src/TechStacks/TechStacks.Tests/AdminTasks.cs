ef6443f28f6da24d7fd9937e624985d0f5867609
Demis Bellot <demis.bellot@gmail.com>
2014-12-18 23:23:24 -0500

    Add Audit History tables for Technology and TechnologyStack

---
+++ src/TechStacks/TechStacks.Tests/AdminTasks.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using TechStacks.ServiceModel.Types;
+
+namespace TechStacks.Tests
+{
+    [Ignore("One-off Admin Tasks")]
+    public class AdminTasks
+    {
+        private IAppSettings config;
+        IAppSettings Config
+        {
+            get { return config ?? (config = new TextFileSettings("~/appsettings.txt".MapProjectPath())); }
+        }
+
+        private IDbConnectionFactory factory;
+        private IDbConnectionFactory Factory
+
+        {
+            get
+            {
+                return factory ?? (factory =  new OrmLiteConnectionFactory(
+                    Config.GetString("OrmLite.ConnectionString"), PostgreSqlDialect.Provider));
+            }
+        }
+
+        public IDbConnection OpenDbConnection()
+        {
+            return Factory.OpenDbConnection();
+        }
+
+        [Test]
+        public void Create_History_Table()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<TechnologyHistory>();
+                db.DropAndCreateTable<TechnologyStackHistory>();
+            }
+        }
+    }
+}
