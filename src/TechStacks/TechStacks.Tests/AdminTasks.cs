6240b48aac7a518b5f04ac1d0647e6f97d349f0e
Demis Bellot <demis.bellot@gmail.com>
2015-12-16 10:33:15 -0800

    remove comments

--- src/TechStacks/TechStacks.Tests/AdminTasks.cs
+++ src/TechStacks/TechStacks.Tests/AdminTasks.cs
@@ -39,62 +39,62 @@ namespace TechStacks.Tests
             return Factory.OpenDbConnection();
         }
 
-        //[Test]
-        //public void Reset_History_Table()
-        //{
-        //    using (var db = OpenDbConnection())
-        //    {
-        //        db.DropAndCreateTable<TechnologyHistory>();
-        //        db.DropAndCreateTable<TechnologyStackHistory>();
+        [Test]
+        public void Reset_History_Table()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<TechnologyHistory>();
+                db.DropAndCreateTable<TechnologyStackHistory>();
 
-        //        var allTechs = db.Select<Technology>();
-        //        foreach (var tech in allTechs)
-        //        {
-        //            var history = tech.ConvertTo<TechnologyHistory>();
-        //            history.Operation = "INSERT";
-        //            history.TechnologyId = tech.Id;
-        //            db.Insert(history);
-        //        }
+                var allTechs = db.Select<Technology>();
+                foreach (var tech in allTechs)
+                {
+                    var history = tech.ConvertTo<TechnologyHistory>();
+                    history.Operation = "INSERT";
+                    history.TechnologyId = tech.Id;
+                    db.Insert(history);
+                }
 
-        //        var allStacks = db.Select<TechnologyStack>();
-        //        foreach (var stack in allStacks)
-        //        {
-        //            var history = stack.ConvertTo<TechnologyStackHistory>();
-        //            history.Operation = "INSERT";
-        //            history.TechnologyStackId = stack.Id;
-        //            history.TechnologyIds = db.Column<long>(db.From<TechnologyChoice>()
-        //                .Where(x => x.TechnologyStackId == stack.Id)
-        //                .Select(x => x.TechnologyId));
-        //            db.Insert(history);
-        //        }
-        //    }
-        //}
+                var allStacks = db.Select<TechnologyStack>();
+                foreach (var stack in allStacks)
+                {
+                    var history = stack.ConvertTo<TechnologyStackHistory>();
+                    history.Operation = "INSERT";
+                    history.TechnologyStackId = stack.Id;
+                    history.TechnologyIds = db.Column<long>(db.From<TechnologyChoice>()
+                        .Where(x => x.TechnologyStackId == stack.Id)
+                        .Select(x => x.TechnologyId));
+                    db.Insert(history);
+                }
+            }
+        }
 
-        //[Test]
-        //public void UpdateSlugTitles()
-        //{
-        //    using (var db = OpenDbConnection())
-        //    {
-        //        var allTechs = db.Select<Technology>();
-        //        allTechs.ForEach(x => x.Slug = x.Name.GenerateSlug());
-        //        db.UpdateAll(allTechs);
-        //        var allStacks = db.Select<TechnologyStack>();
-        //        allStacks.ForEach(x => x.Slug = x.Name.GenerateSlug());
-        //        db.UpdateAll(allStacks);
-        //    }
-        //}
+        [Test]
+        public void UpdateSlugTitles()
+        {
+            using (var db = OpenDbConnection())
+            {
+                var allTechs = db.Select<Technology>();
+                allTechs.ForEach(x => x.Slug = x.Name.GenerateSlug());
+                db.UpdateAll(allTechs);
+                var allStacks = db.Select<TechnologyStack>();
+                allStacks.ForEach(x => x.Slug = x.Name.GenerateSlug());
+                db.UpdateAll(allStacks);
+            }
+        }
 
-        //[Test]
-        //public void Can_Tweet_update()
-        //{
-        //    var twitter = new TwitterUpdates(
-        //        Config.GetString("WebStacks.ConsumerKey"),
-        //        Config.GetString("WebStacks.ConsumerSecret"),
-        //        Config.GetString("WebStacks.AccessToken"),
-        //        Config.GetString("WebStacks.AccessSecret"));
+        [Test]
+        public void Can_Tweet_update()
+        {
+            var twitter = new TwitterUpdates(
+                Config.GetString("WebStacks.ConsumerKey"),
+                Config.GetString("WebStacks.ConsumerSecret"),
+                Config.GetString("WebStacks.AccessToken"),
+                Config.GetString("WebStacks.AccessSecret"));
 
-        //    twitter.Tweet("Test for http://techstacks.io");
-        //}
+            twitter.Tweet("Test for http:techstacks.io");
+        }
 
         [Test]
         public void Import_PageViews()
